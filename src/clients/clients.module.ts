4ffdfd657aa3f17bd9930792a1e43fbba59556f3
Mateus Sousa <sousa.programador@gmail.com>
2026-02-27 14:12:32 -0300

    refactor(clients): register repository using injection token

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { Client, ClientSchema } from './schemas/client.schema';
+import { ClientsRepository } from './repositories/clients.repository';
+import { CLIENTS_REPOSITORY } from './repositories/clients.repository.token';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Client.name, schema: ClientSchema }]),
   ],
   controllers: [ClientsController],
-  providers: [ClientsService],
+  providers: [
+    ClientsService,
+    {
+      provide: CLIENTS_REPOSITORY,
+      useClass: ClientsRepository,
+    },
+  ],
 })
 export class ClientsModule {}
